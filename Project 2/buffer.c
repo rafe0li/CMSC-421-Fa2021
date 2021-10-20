470f40055782e707c38f8bd599ed2f9f0ef6d37f
Rafael Li <01rafe0li@gmail.com>
2021-10-20 15:17:32 -0400

    No runtime memory errors Fixed memory in init_buffer_421 Issues with insertion

--- Project 2/buffer.c
+++ Project 2/buffer.c
@@ -1,36 +1,59 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "buffer.h"
-struct ring_buffer_421 BUFF = malloc(sizeof(struct ring_buffer_421_t));
-struct node_421* HEAD = (struct node_421*)malloc(sizeof(struct node_421_t));
-struct node_421* TAIL = (struct node_421*)malloc(sizeof(struct node_421_t));
+struct ring_buffer_421 BUFF;
 
-
+// Commented return conditions for syscall failing
 long init_buffer_421() {
 	int i;
-	struct node_421* curr = (struct node_421*)malloc(sizeof(struct node_421_t));
+	struct node_421* curr = (struct node_421*)malloc(sizeof(node_421_t));
 	struct node_421* prev;
+
+	// Buffer already initialized
+	//if (BUFF.read != NULL) {
+		//return -1;
+	//}
+
 	for (i = 0; i < SIZE_OF_BUFFER; i++) {
-		curr.data = 0;
-		curr->curr = NULL;
+		curr->data = 0;
+		curr->next = NULL;
 		if (i == 0) {
-			HEAD = curr;
+			BUFF.read = (struct node_421*)malloc(sizeof(node_421_t));
+			BUFF.read = curr;
+			BUFF.write = (struct node_421*)malloc(sizeof(node_421_t));
+			BUFF.write = curr;
 		}
 		else {
-			if (i == SIZE_OF_BUFFER - 1) {
-				TAIL = curr;
-			}
-			prev->curr = curr;
-			prev = curr;
+			prev->next = curr;
 		}
-		curr = (struct node_421*)realloc(curr, sizeof(struct node_421_t));
+		prev = curr;
+		// Newly created node
+		struct node_421* n_node = (struct node_421*)malloc(sizeof(node_421_t));
+		curr = n_node;
 	}
+
+	//return 0;
 }
 
 long insert_buffer_421(int i) {
-
+	// Buffer full
+	//if (BUFF.read == BUFF.write)
+		//return -1;
+	BUFF.write->data = i;
+	BUFF.write = BUFF.write->next;
+	BUFF.length++;
+	//return 0;
 }
 
 long print_buffer_421() {
+	int i;
+	struct node_421* curr = (struct node_421*)malloc(sizeof(struct node_421));
+	curr = BUFF.read;
 
+	for (i = 0; i < SIZE_OF_BUFFER; i++) {
+		printf("[%d]\t", curr->data);
+		curr = curr->next;
+	}
 }
 
 long delete_buffer_421() {
@@ -39,5 +62,6 @@ long delete_buffer_421() {
 
 int main() {
 	init_buffer_421();
+	print_buffer_421();
 	return 0;
 }
